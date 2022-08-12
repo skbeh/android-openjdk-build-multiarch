ebe16d634a6981bb4e865db903afb4a11aecfefe
skbeh <60107333+skbeh@users.noreply.github.com>
2022-08-13 02:29:53 +0800

    update

--- buildjdk.sh
+++ buildjdk.sh
@@ -81,7 +81,7 @@ bash ./configure \
   --x-libraries=/usr/lib \
   ${platform_args:-} ||
   error_code=$?
-if [ "$error_code" -ne 0 ]; then
+if [ "${error_code:-0}" -ne 0 ]; then
   echo "\n\nCONFIGURE ERROR $error_code , config.log:"
   cat config.log
   exit $error_code
@@ -90,7 +90,7 @@ fi
 cd build/${JVM_PLATFORM}-${TARGET_JDK}-${JVM_VARIANTS}-${JDK_DEBUG_LEVEL}
 make JOBS=4 images ||
   error_code=$?
-if [ "$error_code" -ne 0 ]; then
+if [ "${error_code:-0}" -ne 0 ]; then
   echo "Build failure, exited with code $error_code. Trying again."
   make JOBS=4 images
 fi
